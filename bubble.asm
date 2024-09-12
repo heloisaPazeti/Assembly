611a481079748ef278cb58c385ffc32f2f6a39f0
heloisaPazeti <helopazeti@gmail.comm>
2024-09-12 23:40:20 +0000

    1

--- bubble.asm
+++ bubble.asm
@@ -12,14 +12,27 @@ main:	addi s0, zero, 7				# s0 = max = 7
 		addi t1, zero, 6				# t1 = j = max-1
 		addi t2, t0, 1					# t2 = t0 + 1 = i + 1
 
-loop1:	bge t1, t2, fimLoop1			# j >= i+1i
+
+loop1:	bgt t1, t2, fimLoop1			# j >= i+1i
 
 		addi t3, t1, -1					# t3 = j-1
-		blt (t3)nums, (t1)nums, outIf
+
+		la s3, nums						# s3 = &nums
+		addi a1, zero, 4				# a1 = 4
+
+		mul a2, a1, t3					# a2 = 4*(j-1)
+		add s3, s3, a2					# &s3 = &num[j-1]
+
+
+		lw s1, 0(s3)					# s1 = num[j-1]
+		addi s3, s3, 4					# &s3 = &num[j]
+		lw s2, 0(s3)					# s2 = num[j]
+
+		blt s1, s2, outIf
 		
-		add a0, zero, (t3)nums			# a0 = nums[j-1]
-		add (t3)nums, zero, (t1)nums	# nums[j-1] = nums[j]
-		add (t1)nums, zero, a0 			# nums[j] = a0
+		add a0, zero, s1				# a0 = nums[j-1]
+		add s1, zero, s2				# nums[j-1] = nums[j]
+		add s2, zero, a0 				# nums[j] = a0
 
 		
 		addi t1, t1, -1
@@ -31,25 +44,40 @@ outIf:
 	j loop1
 		 
 fimLoop1:
-
-		bgt t0, 6, print		# se i > 6 -> sai dos loops
-		addi t0, t0, 1			# i++
-		addi t2, t0, 1			# t2 = t1 +1
+		addi a0, zero, 6
+		bgt t0, a0, print				# se i > 6 -> sai dos loops
+		addi t0, t0, 1					# i++
+		addi t2, t0, 1					# t2 = t1 +1
 		j loop1
 
 print: 
 
-		addi s1, zero, 0		# i = 0
-		bge s1, 7, fimProg		# if(i >= 7) end
+		addi s1, zero, 0				# i = 0
+		addi a1, zero, 4				# a1 = 4
+		la s3, nums						# s3 = &nums
 
-		addi a7, zero, 4	 	# printa numero
-		la a0,(s1)nums			# printa nums[i]
+printLoop:
+
+		bge s1, s0, fimProg				# if(i >= 7) end
+
+		mul a2, a1, s1					# a2 = 4*i
+		add s3, s3, a2					# &s3 = &num[i]
+		lw s2, 0(s3)					# s2 = num[i]
+		
+		addi a7, zero, 1	 			# printa nums[i]
+		mv a0, s2						# mv -> Move Value
 		ecall
 
-		addi s1, s1, 1			# i++
-		j print
+		addi s1, s1, 1					# i++
+		j printLoop
 
 fimProg:
 
-		addi a7, zero, 10		# encerra o programa
+		addi a7, zero, 10				# encerra o programa
 		ecall
+
+
+
+# a0 - a1 - a2
+# t0 - t1 - t2 - t3
+# s0 - s1 - s2 - s3
